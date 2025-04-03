fa8c3f7f54a686c650e53e6e4b2107663f94e0e7
Doris-vizie <dorisvizie@gmail.com>
2025-04-03 06:47:54 +0300

    finished setting up

---
+++ lib/supabase_config.dart
@@ -0,0 +1,15 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://uzbmwmfgmpjmcsappeyv.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6Ym13bWZnbXBqbWNzYXBwZXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDQ0ODQsImV4cCI6MjA1OTE4MDQ4NH0.Nfi9ufF4JG82EGkqAFwkJ-WiCQzhrzU0HEUaFyo1DOM';
+
+  static Future<void> initialize() async {
+    await Supabase.initialize(
+      url: supabaseUrl,
+      anonKey: supabaseAnonKey,
+    );
+  }
+
+  static SupabaseClient get client => Supabase.instance.client;
+}
